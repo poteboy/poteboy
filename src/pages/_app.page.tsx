1c3172ae474e361ae867513209acbff72ff41400
Keita Furuse <59927325+poteboy@users.noreply.github.com>
2022-05-30 17:29:45 +0900

    add favicon (#2)

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -18,6 +18,35 @@ function MyApp({ Component, pageProps }: AppProps) {
         <meta property="og:site_name" content="ケンコウイチバン" />
         {/* <meta property="og:url" content="https://poteboy.com/" /> */}
         <meta property="og:title" content="ケンコウイチバン" />
+        {/* Favicon */}
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href={require('@src/public/favicons/apple-touch-icon.png')}
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href={require('@src/public/favicons/favicon-32x32.png')}
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href={require('@src/public/favicons/favicon-16x16.png')}
+        />
+        <link rel="manifest" href="/favicons/site.webmanifest" />
+        <link
+          rel="mask-icon"
+          href={require('@src/public/favicons/safari-pinned-tab.svg')}
+          color="#000000"
+        />
+        <link
+          rel="shortcut icon"
+          href={require('@src/public/favicons/favicon.ico')}
+        />
+        <meta name="theme-color" content="#ffffff" />
       </Head>
       <ChakraProvider theme={theme}>
         <Component {...pageProps} />
