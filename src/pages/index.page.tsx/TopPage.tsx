5eebab99d190f1c2a825cb895df9fa42108b293a
poteboy <poteboy.dev@gmail.com>
2022-12-25 13:07:51 +0900

    top page

--- src/pages/index.page.tsx/TopPage.tsx
+++ src/pages/index.page.tsx/TopPage.tsx
@@ -1,8 +1,5 @@
-import { useMutation } from "@src/utils";
-import { useForm } from "react-hook-form";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { CreateUserInput, createUserSchema } from "@src/schema";
-import { Box, Image, Container } from "@chakra-ui/react";
+import Link from "next/link";
+import { Box, Image, Container, Spacer } from "@chakra-ui/react";
 import { useEffect } from "react";
 import { TextInput } from "@src/components";
 import { colors } from "@src/styles";
@@ -11,12 +8,15 @@ export default function Home() {
   return (
     <Box flex={1} minH="100vh" bg={colors.baseBg}>
       <Container as="main">
-        <Image
-          src="https://pbs.twimg.com/profile_images/1571474754976219136/RN77fkuW_400x400.jpg"
-          alt=""
-          w="100px"
-          borderRadius="50%"
-        />
+        <Spacer h="80px" />
+        <Link href="/">
+          <Image
+            src="https://pbs.twimg.com/profile_images/1571474754976219136/RN77fkuW_400x400.jpg"
+            alt=""
+            w="100px"
+            borderRadius="50%"
+          />
+        </Link>
       </Container>
     </Box>
   );
