f6ef7a4177ca08b7819c9b1ec2a4f1be9b321d52
poteboy <poteboy.dev@gmail.com>
2022-12-30 02:41:20 +0900

    check if this works

--- src/utils/cors.ts
+++ src/utils/cors.ts
@@ -3,7 +3,11 @@ import Cors from "cors";
 
 const cors = Cors();
 
-function runMiddleware(req: NextApiRequest, res: NextApiResponse, fn: any) {
+function runMiddleware(
+  req: NextApiRequest,
+  res: NextApiResponse,
+  fn: typeof cors
+) {
   return new Promise((resolve, reject) => {
     fn(req, res, (result: any) => {
       if (result instanceof Error) {
@@ -17,17 +21,18 @@ function runMiddleware(req: NextApiRequest, res: NextApiResponse, fn: any) {
 
 export function withCors(handler: NextApiHandler) {
   return async (req: NextApiRequest, res: NextApiResponse) => {
-    await runMiddleware(req, res, cors);
-    req.headers["access-control-allow-origin"] = "*";
-    req.headers["access-control-allow-headers"] =
-      "Origin, X-Requested-With, Content-Type, Accept, Authorization";
-
     res.setHeader("access-control-allow-origin", "*");
     res.setHeader(
       "access-control-allow-headers",
       "Origin, X-Requested-With, Content-Type, Accept, Authorization"
     );
     res.setHeader("credentials", "include");
+
+    if (req.method === "OPTIONS") {
+      res.writeHead(200);
+      return res.end();
+    }
+    await runMiddleware(req, res, cors);
 
     return await handler(req, res);
   };
